9f42c1c160c87143b084634f1854244a281cdf11
Ramy Kronby <ramykronby@gmail.com>
2025-02-08 18:18:38 +0200

    ds4

--- src/ds4_control/ds4.py
+++ src/ds4_control/ds4.py
@@ -1,4 +1,5 @@
 import time
+import serial
 from pyPS4Controller.controller import Controller
 
 
@@ -6,39 +7,40 @@ class MyController(Controller):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._up = False  # Flag to control the loop
-        self._down = False  # Flag to control the loop
-        self._stop = False  # Flag to control the loop
+        self.ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=1)  # Serial connection
 
-    def send_signal(self):
-        """Continuously sends signal while Up Arrow is pressed"""
-        if self._up:
-            print("send up")
-            time.sleep(0.1)
+    def send_signal(self, message):
+        """Continuously sends signal while a button is pressed"""
+        if self._stop:
+            message = "s"
+        elif self._up:
+            message = "u"
         elif self._down:
-            print("send down")
-            time.sleep(0.1)
-        elif self._stop:
-            print("stop")
-            time.sleep(0.1)
+            message = "d"
+        print(f"Sending: {message}")
+        self.ser.write(f"{message}\n".encode("utf-8"))  # Send over serial
+        time.sleep(0.1)  # Adjust signal frequency
 
     def on_up_arrow_press(self):
+        """Start sending 'u' when Up Arrow is pressed"""
         self._up = True
         self._down = False
         self._stop = False
-        self.send_signal()
+        self.send_signal("u")
 
     def on_down_arrow_press(self):
+        """Start sending 'd' when Down Arrow is pressed"""
         self._up = False
         self._down = True
         self._stop = False
-        self.send_signal()
+        self.send_signal("d")
 
     def on_up_down_arrow_release(self):
+        """Stop sending signals when button is released"""
         self._up = False
         self._down = False
         self._stop = True
-        self.send_signal()
+        self.send_signal("s")
 
     # Override other event handlers to do nothing
     def on_any_press(self, button_id=None):
@@ -89,4 +91,11 @@ class MyController(Controller):
 
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.listen(timeout=60)
+
+
+def main():
+    controller.listen(timeout=60)  # Keeps running until timeout
+
+
+if __name__ == "__main__":
+    main()

d5343241d31cf78132ac07f73c926c32403d9048
Ramy Kronby <ramykronby@gmail.com>
2025-02-08 16:01:06 +0200

    ds4

--- src/ds4_control/ds4.py
+++ src/ds4_control/ds4.py
@@ -28,6 +28,45 @@ class MyController(Controller):
     def on_any_release(self, button_id=None):
         pass
 
+    def on_R3_y_at_rest(self):
+        pass
+
+    def on_R3_right(self, value):
+        pass
+
+    def on_L3_up(self, value):
+        pass
+
+    def on_L3_right(self, value):
+        pass
+
+    def on_L3_left(self, value):
+        pass
+
+    def on_R3_down(self, value):
+        pass
+
+    def on_R3_up(self, value):
+        pass
+
+    def on_L3_down(self, value):
+        pass
+
+    def on_R3_left(self, value):
+        pass
+
+    def on_R3_x_at_rest(self):
+        pass
+
+    def on_R3_y_at_rest(self):
+        pass
+
+    def on_L3_x_at_rest(self):
+        pass
+
+    def on_L3_y_at_rest(self):
+        pass
+
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 controller.listen(timeout=60)

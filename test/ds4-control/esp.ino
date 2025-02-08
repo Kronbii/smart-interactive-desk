d0a57db459056ca7cac0c184e95b96aec621cf4a
Ramy Kronby <ramykronby@gmail.com>
2025-02-08 22:47:34 +0200

    ds4

--- test/ds4-control/esp.ino
+++ test/ds4-control/esp.ino
@@ -1,43 +1,61 @@
 #include <HardwareSerial.h>
-
-#define RXD2 16  // GPIO16 as RX
-#define TXD2 17  // GPIO17 as TX
+// Define pins
+#define RPWM 15   // Right PWM
+#define LPWM 2  // Left PWM
+#define RXD2 16
+#define TXD2 17
 #define LED_BUILTIN 2
 
-HardwareSerial laptop(2); // Use Serial2
+HardwareSerial laptop(2);
+
+bool dir = false;
+bool move = false;
 
 void setup() {
-  Serial.begin(115200);  // Initialize Serial Monitor for debugging
-  laptop.begin(115200, SERIAL_8N1, RXD2, TXD2); // Initialize Serial2 with defined pins
-  laptop.println("ESP32 Ready to communicate with Raspberry Pi");
+    pinMode(RPWM, OUTPUT);
+    pinMode(LPWM, OUTPUT);
+    Serial.begin(115200);  // Start serial communication
+    laptop.begin(115200, SERIAL_8N1, RXD2, TXD2);
+
+    // Ensure motor is stopped at startup
+    digitalWrite(RPWM, LOW);
+    digitalWrite(LPWM, LOW);
 }
 
 void loop() {
-  if (laptop.available() > 0) {
-    String input = laptop.readString();
-    input.trim(); // Trim the input
-    if (input == "u") {
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(1000); // Adjust delay as needed
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-    else if (input == "d") {
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(1000); // Adjust delay as needed
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-    else if (input == "s") {
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(1000); // Adjust delay as needed
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-  }
-
   if (Serial.available() > 0) {
-    String dataFromPi = Serial.readStringUntil('\n');
-    if (dataFromPi.length() > 0) {
-      laptop.println(dataFromPi);
-    }
+      String ramy = Serial.readString();
+      ramy.trim();
+      //laptop.println(ramy);
+      if (ramy == "u") {
+          move = true;
+          dir = true;
+          //laptop.println("UP");
+      } else if (ramy == "d") {
+          move = true;
+          dir = false;
+      } else if (ramy == "s") {
+          move = false;
+          dir = false;
+      }
+      else {
+          laptop.println(ramy);
+      }
   }
-
 }
+
+  // Move forward
+  if (move && dir){
+    digitalWrite(RPWM, HIGH);
+    digitalWrite(LPWM, HIGH);
+  }
+  // Move backward
+  else if (move && !dir){
+    digitalWrite(RPWM, LOW);
+    digitalWrite(LPWM, LOW);
+  }
+  // Stop
+  else {
+    digitalWrite(RPWM, LOW);
+    digitalWrite(LPWM, LOW);
+  }

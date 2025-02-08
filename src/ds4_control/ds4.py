f95203ad08cb7a4df9db31a34bf57a935fd1bb1e
Ramy Kronby <ramykronby@gmail.com>
2025-02-08 15:59:22 +0200

    ds4

--- src/ds4_control/ds4.py
+++ src/ds4_control/ds4.py
@@ -1,3 +1,4 @@
+import time
 from pyPS4Controller.controller import Controller
 
 
@@ -5,56 +6,26 @@ class MyController(Controller):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.running = False  # Flag to control the signal loop
 
-    # Only handle X button press
     def on_x_press(self):
         print("X pressed: fet ya bassam")
+        self.running = True
 
-    # Only handle X button release
+        # Run the continuous signal loop while button is pressed
+        while self.running:
+            print("Signal is being sent...")
+            time.sleep(0.1)  # Adjust frequency of the signal
+
     def on_x_release(self):
         print("X released: dahar ya bassam")
-        
-    def on_up_arrow_press(self):
-        return super().on_up_arrow_press()
+        self.running = False  # Stop the loop when button is released
 
     # Override other event handlers to do nothing
-    def on_R3_y_at_rest(self):
+    def on_any_press(self, button_id=None):
         pass
 
-    def on_R3_right(self, value):
-        pass
-
-    def on_L3_up(self, value):
-        pass
-
-    def on_L3_right(self, value):
-        pass
-
-    def on_L3_left(self, value):
-        pass
-
-    def on_R3_down(self, value):
-        pass
-
-    def on_R3_up(self, value):
-        pass
-
-    def on_L3_down(self, value):
-        pass
-
-    def on_R3_left(self, value):
-        pass
-
-    def on_R3_x_at_rest(self):
-        pass
-
-    def on_R3_y_at_rest(self):
-        pass
-
-    def on_L3_x_at_rest(self):
-        pass
-
-    def on_L3_y_at_rest(self):
+    def on_any_release(self, button_id=None):
         pass
 
 

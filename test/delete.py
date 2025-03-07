0c89721d5ef7bf5f40ba2c290eb9ba1f0e7de267
kronbii <ramykronby@gmail.com>
2025-03-08 01:49:18 +0200

    pi

---
+++ test/delete.py
@@ -0,0 +1,14 @@
+import serial  # for connection with the serial port
+
+# To connect to the serial port
+import serial.tools.list_ports
+
+
+ports = list(serial.tools.list_ports.comports())  # outputs all the ports connected
+found_port = list()  # list of all the ports connected
+
+for i in range(len(ports)):
+    print(ports[i])  # to print all the available ports connected
+    if "USB" in ports[i].device:
+        found_port.append(ports[i].device)  # append all the ports which have serial word in them
+        print(found_port[i])

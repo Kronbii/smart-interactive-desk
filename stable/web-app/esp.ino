4ecf4898f8e095de087a18b419fda30cf41c3d13
kronbii <ramykronby@gmail.com>
2025-03-30 01:54:04 +0300

    static ip steps

--- stable/web-app/esp.ino
+++ stable/web-app/esp.ino
@@ -1,110 +1,135 @@
 #include <HardwareSerial.h>
 
-//TODO: make tilting and up-down work together
-// Commuinication pins between ESP32 and laptop
-#define RXD2 16
-#define TXD2 17
-
 // Stepper motor pins
-#define STEP1 22
-#define DIR1 23
-#define STEP2 19
-#define DIR2 18
+#define M1IN1 5
+#define M1IN2 4
+#define M2IN1 3
+#define M2IN2 2
 
-// Stepper motor configuration
-#define STEP_DELAY 500  // Microseconds delay between steps
-#define STEPS_PER_CYCLE 200  // Adjust steps as needed
+#define btmSwitch 19  // Bottom switch pin
 
-#define RPWM 14   // Right PWM
-#define LPWM 12    // Left PWM
-
-// Initialize serial communication between esp and laptop
-HardwareSerial laptop(2);
+#define RPWM 7   // Right PWM
+#define LPWM 6   // Left PWM
 
 // Direction and movement flags
-bool vertical_move = false;
-bool tilt_move = false;
-bool vertical_dir = false;
-bool tilt_dir = false;
+bool move_up_flag = false;
+bool move_down_flag = false;
+bool tilt_up_flag = false;
+bool tilt_down_flag = false;
+bool stop_motion_flag = true;
+bool btm_limit_flag = false;  // Only the bottom limit flag now
 
+// Debouncing variables
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 200; // Debounce delay (in milliseconds)
 
 void setup() {
-    pinMode(STEP1, OUTPUT);
-    pinMode(DIR1, OUTPUT);
-    pinMode(STEP2, OUTPUT);
-    pinMode(DIR2, OUTPUT);
+    pinMode(M1IN1, OUTPUT);
+    pinMode(M1IN2, OUTPUT);
+    pinMode(M2IN1, OUTPUT);
+    pinMode(M2IN2, OUTPUT);
     
     pinMode(RPWM, OUTPUT);
     pinMode(LPWM, OUTPUT);
 
-    Serial.begin(115200);  // Start serial communication
-    laptop.begin(115200, SERIAL_8N1, RXD2, TXD2);
+    pinMode(btmSwitch, INPUT_PULLUP); // Set bottom switch pin as input with internal pull-up resistor
+
+    Serial2.begin(115200);
+    Serial.begin(115200);    // Main serial communication
+
+    attachInterrupt(digitalPinToInterrupt(btmSwitch), btm_int, FALLING);  // Trigger on pin 19 falling edge (button press)
 
     stop_table();
+    Serial.println("Begin of operations");
 }
 
 void loop() {
+    // Handle bottom limit switch interrupt
+    if (btm_limit_flag) {
+        Serial.println("Bottom Interrupt Registered");
+        stop_table();  // Stop all movements if the table is frozen
+        delay(1000);   // Wait for a moment
+        move_table_up();
+        delay(2000);   // Wait for a moment
+        stop_table();
+        btm_limit_flag = false;
+    }
 
-    if (Serial.available() > 0) {
-        String ramy = Serial.readStringUntil('\n');  // Read full command
+    // Read and handle commands from Serial2 (remote control)
+    if (Serial2.available() > 0) {
+        String ramy = Serial2.readStringUntil('\n');  // Read full command
         ramy.trim();  // Remove whitespace and newline
 
-        if (ramy == "u") {         // Move forward
-            vertical_move = true;
-            vertical_dir = true;
-            tilt_move = false;
-            tilt_dir = false;
-            laptop.println(ramy); // Debugging output
-        } else if (ramy == "d") {  // Move backward
-            vertical_move = true;
-            vertical_dir = false;
-            tilt_move = false;
-            tilt_dir = false;
-            laptop.println(ramy); // Debugging output
-        } else if (ramy == "s") {  // Stop
-            vertical_move = false;
-            tilt_move = false;
-            vertical_dir = false;
-            tilt_dir = false;
-            laptop.println(ramy); // Debugging output
+        if (ramy == "u") {
+            move_up_flag = true;
+            move_down_flag = false;
+            tilt_up_flag = false;
+            tilt_down_flag = false;
+            stop_motion_flag = false;
+            Serial.println(ramy);
+        } else if (ramy == "d") {
+            move_up_flag = false;
+            move_down_flag = true;
+            tilt_up_flag = false;
+            tilt_down_flag = false;
+            stop_motion_flag = false;
+            Serial.println(ramy);
+        } else if (ramy == "s") {
+            move_up_flag = false;
+            move_down_flag = false;
+            tilt_up_flag = false;
+            tilt_down_flag = false;
+            stop_motion_flag = true;
+            Serial.println(ramy);
         } else if (ramy == "tu") {
-            vertical_move = false;
-            vertical_dir = false;
-            tilt_move = true;
-            tilt_dir = true;
-            laptop.println(ramy); // Debugging output
-        } else if (ramy == "td"){
-            vertical_move = false;
-            vertical_dir = false;
-            tilt_move = true;
-            tilt_dir = false;
-            laptop.println(ramy); // Debugging output
+            move_up_flag = false;
+            move_down_flag = false;
+            tilt_up_flag = true;
+            tilt_down_flag = false;
+            stop_motion_flag = false;
+            Serial.println(ramy);
+        } else if (ramy == "td") {
+            move_up_flag = false;
+            move_down_flag = false;
+            tilt_up_flag = false;
+            tilt_down_flag = true;
+            stop_motion_flag = false;
+            Serial.println(ramy);
         } else {
-            vertical_move = false;
-            vertical_dir = false;
-            tilt_move = false;
-            tilt_dir = false;
-            laptop.println("Unknown command: " + ramy); // Debugging output
+            move_up_flag = false;
+            move_down_flag = false;
+            tilt_up_flag = false;
+            tilt_down_flag = false;
+            stop_motion_flag = true;
+            Serial.println("Unknown command: " + ramy);
         }
     }
 
-if (vertical_move && vertical_dir) {
-    move_table_up();
-}
-else if (vertical_move && !vertical_dir) {
+    // Execute movements based on flags
+    if (move_up_flag && !stop_motion_flag) {
+        move_table_up();
+    } else if (move_down_flag && !stop_motion_flag) {
         move_table_down();
+    } else if (tilt_up_flag && !stop_motion_flag) {
+        tilt_table_up();
+    } else if (tilt_down_flag && !stop_motion_flag) {
+        tilt_table_down();
+    } else {
+        stop_table();
     }
-else if (tilt_move && tilt_dir) {
-    tilt_table_cw();
-}
-else if (tilt_move && !tilt_dir) {
-    tilt_table_ccw();
-}
-else {
-    stop_table();
-}
 }
 
+// Bottom limit switch interrupt (with debouncing)
+void btm_int() {
+    unsigned long currentMillis = millis();
+    if (currentMillis - lastDebounceTime > debounceDelay) {  // Only trigger if debounce time has passed
+        Serial.println("Bottom Interrupt Function Entered");
+        btm_limit_flag = true;
+        lastDebounceTime = currentMillis;  // Update last debounce time
+    }
+}
+
+// Motor control functions
 void move_table_up() {
     digitalWrite(RPWM, HIGH);
     digitalWrite(LPWM, LOW);
@@ -118,26 +143,22 @@ void move_table_down() {
 void stop_table() {
     digitalWrite(RPWM, LOW);
     digitalWrite(LPWM, LOW);
+    digitalWrite(M1IN1, LOW);
+    digitalWrite(M1IN2, LOW);
+    digitalWrite(M2IN1, LOW);
+    digitalWrite(M2IN2, LOW);
 }
 
-void tilt_table_cw() {
-    digitalWrite(DIR1, HIGH);
-    digitalWrite(DIR2, HIGH);
-    digitalWrite(STEP1, HIGH);
-    digitalWrite(STEP2, HIGH);
-    delayMicroseconds(STEP_DELAY);
-    digitalWrite(STEP1, LOW);
-    digitalWrite(STEP2, LOW);
-    delayMicroseconds(STEP_DELAY);
+void tilt_table_up() {
+    digitalWrite(M1IN1, HIGH);
+    digitalWrite(M1IN2, LOW);
+    digitalWrite(M2IN1, HIGH);
+    digitalWrite(M2IN2, LOW);
 }
 
-void tilt_table_ccw() {
-    digitalWrite(DIR1, LOW);
-    digitalWrite(DIR2, LOW);
-    digitalWrite(STEP1, HIGH);
-    digitalWrite(STEP2, HIGH);
-    delayMicroseconds(STEP_DELAY);
-    digitalWrite(STEP1, LOW);
-    digitalWrite(STEP2, LOW);
-    delayMicroseconds(STEP_DELAY);
+void tilt_table_down() {
+    digitalWrite(M1IN1, LOW);
+    digitalWrite(M1IN2, HIGH);
+    digitalWrite(M2IN1, LOW);
+    digitalWrite(M2IN2, HIGH);
 }
